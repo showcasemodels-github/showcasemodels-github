dc1ca3e4d46be27d37785235bfb6bd21ccdc096b
jephjeph <joseph.reyes@starfi.sh>
2013-05-29 10:59:15 +0800

    application products

--- Project/1_Website/Code/Applications/Products/Ajax/productsAjaxController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Project/ApplicationsFramework/MVC_superClasses/applicationsSuperController.php';
-//----------------------------------------------------------
-
-class productsAjaxController extends applicationsSuperController
-{
-
-}
